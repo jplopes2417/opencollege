f6c63c5a28476a7c698609d206eb253384169771
Jonathan Lopes <jplopes3518@outlook.com>
2022-08-20 13:43:26 -0300

    WIP: first commit

---
+++ src/main/java/com/collegemongosystem/opencollege/OpencollegeApplication.java
@@ -0,0 +1,13 @@
+package com.collegemongosystem.opencollege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpencollegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpencollegeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collegemongosystem/opencollege/OpencollegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegemongosystem.opencollege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpencollegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
